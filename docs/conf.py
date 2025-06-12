12053c104f260cd5f014b0145df1b3732861aa8a
weclean-slo <weclean88@outlook.com>
2025-06-12 10:56:25 +0200

    Add files via upload

---
+++ docs/conf.py
@@ -0,0 +1,15 @@
+# Configuration file for the Sphinx documentation builder.
+
+project = 'Čiščenje fasade'
+copyright = '2025'
+author = 'Weclean'
+
+release = '1.0'
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,10 @@
+Čiščenje fasade documentation
+=============================
+
+Dobrodošli v dokumentaciji za profesionalno čiščenje fasad.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Vsebina:
+
+   (še ni dodatnih vsebin)
